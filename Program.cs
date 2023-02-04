25f9894d17183d2a9b6f2ab98eec8ad6fe713489
Michel Gutner <michel.gutner@sanar.com>
2023-02-04 19:46:53 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Blog.Models;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ReadUsers();
+
+        const string CONNECTION_STRING = "Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True;";
+
+        static void ReadUsers()
+        {
+
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var users = connection.GetAll<User>();
+
+                foreach (var user in users)
+                {
+                    Console.WriteLine(user.Name);
+                }
+            }
+        }
+    }
+}
