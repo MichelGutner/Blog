31f4d38fc91b072a03428a50997641b83aadf526
Michel Gutner <michel.gutner@sanar.com>
2023-02-04 20:56:32 -0300

    feat: add user repository and embeeded code connection

--- Program.cs
+++ Program.cs
@@ -1,27 +1,88 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Blog.Models;
+using Blog.Repositories;
 using Dapper.Contrib.Extensions;
 using Microsoft.Data.SqlClient;
 
 internal class Program
 {
+    private const string CONNECTION_STRING = "Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True;";
     private static void Main(string[] args)
     {
-        ReadUsers();
+        var connection = new SqlConnection(CONNECTION_STRING);
+        using (connection)
+        {
+            ReadUsers(connection);
+            // ReadUser();
+            // CreateUser();
+            // UpdateUser();
+            // DeleteUser();
+        }
 
-        const string CONNECTION_STRING = "Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True;";
 
-        static void ReadUsers()
+        static void ReadUsers(SqlConnection connection)
+        {
+            var repository = new UserRepository(connection);
+            var users = repository.GetUsers();
+            foreach (var user in users)
+                Console.WriteLine(user.Name);
+        }
+
+        static void ReadUser()
         {
 
             using (var connection = new SqlConnection(CONNECTION_STRING))
             {
-                var users = connection.GetAll<User>();
+                var user = connection.Get<User>(1);
 
-                foreach (var user in users)
-                {
-                    Console.WriteLine(user.Name);
-                }
+                Console.WriteLine(user.Name);
+            }
+        }
+
+        static void CreateUser()
+        {
+            var user = new User()
+            {
+                Name = "Gabriella Augusto Micheletti",
+                Email = "gabriella@gmail.com",
+                PasswordHash = "123456",
+                Bio = "Esposa do Michel",
+                Image = "http://",
+                Slug = "gabi-micheletti"
+            };
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                connection.Insert<User>(user);
+                Console.WriteLine("Cadastro realizado com sucesso!");
+            }
+        }
+
+        static void UpdateUser()
+        {
+            var user = new User()
+            {
+                Id = 2,
+                Name = "Gabriella Micheletti",
+                Email = "gabriella_micheletti@gmail.com",
+                PasswordHash = "123456",
+                Bio = "Estudante de Tecnologia,",
+                Image = "http://",
+                Slug = "gabi-micheletti"
+            };
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                connection.Update<User>(user);
+                Console.WriteLine("Cadastro atualizado com sucesso!");
+            }
+        }
+
+        static void DeleteUser()
+        {
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var user = connection.Get<User>(2);
+                connection.Delete<User>(user);
+                Console.WriteLine("Cadastro excluído com sucesso!");
             }
         }
     }
