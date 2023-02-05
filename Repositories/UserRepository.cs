ae10aac138eb8335570657b198a6537c34a216a4
Michel Gutner <michel.gutner@sanar.com>
2023-02-04 21:22:16 -0300

    feat: add new methods to user repository

--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -10,6 +10,27 @@ namespace Blog.Repositories
     public UserRepository(SqlConnection connection) => _connection = connection;
         public IEnumerable<User> GetUsers() => _connection.GetAll<User>();
         public User GetUser(int id) => _connection.Get<User>(id);
-        public void Create(User user) => _connection.Insert<User>(user);
+        public void Create(User user) {
+            user.Id = 0;
+            _connection.Insert<User>(user);
+        }
+
+        public void Update(User user) {
+            if(user.Id != 0)
+            _connection.Update<User>(user);
+        }
+
+        public void Delete(User user)
+        {
+            if (user.Id != 0)
+                _connection.Delete<User>(user);
+        }
+        public void Delete(int id){
+            if(id != 0)
+                return;
+            
+            var user = _connection.Get<User>(id);
+            _connection.Delete<User>(user);
+        }
     }
 }
