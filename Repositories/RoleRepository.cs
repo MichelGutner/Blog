080318545513ce64052ed080d9123d0f285409a0
Michel Gutner <michel.gutner@sanar.com>
2023-02-04 21:21:54 -0300

    feat: add role repository

---
+++ Repositories/RoleRepository.cs
@@ -0,0 +1,34 @@
+using Blog.Models;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+namespace Blog.Repositories
+{
+    public class RoleRepository {
+        private readonly SqlConnection _connection;
+        public RoleRepository(SqlConnection connection) => _connection = connection;
+        public IEnumerable<Role> GetRoles() => _connection.GetAll<Role>();
+        public Role GetRole(int id) => _connection.Get<Role>(id);
+        public void Create(Role role) => _connection.Insert<Role>(role);
+
+        public void Update(Role role)
+        {
+            if (role.Id != 0)
+                _connection.Update<Role>(role);
+        }
+
+        public void Delete(Role role)
+        {
+            if (role.Id != 0)
+                _connection.Delete<Role>(role);
+        }
+        public void Delete(int id)
+        {
+            if (id != 0)
+                return;
+
+            var role = _connection.Get<Role>(id);
+            _connection.Delete<Role>(role);
+        }
+    }
+}
