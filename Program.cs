79e13b3e9e457e32b45daec2e0db80d8b061b81c
Michel Gutner <michel.gutner@sanar.com>
2023-02-07 22:43:43 -0300

    feat: add initial template screens

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Blog;
 using Blog.Models;
 using Blog.Repositories;
+using Blog.Screens.TagScreens;
 using Dapper.Contrib.Extensions;
 using Microsoft.Data.SqlClient;
 
@@ -9,33 +11,89 @@ internal class Program
     private const string CONNECTION_STRING = "Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True;";
     private static void Main(string[] args)
     {
-        var connection = new SqlConnection(CONNECTION_STRING);
-        using (connection)
+        Database.Connection = new SqlConnection(CONNECTION_STRING);
+        using (Database.Connection)
         {
-            ReadUsers(connection);
-            ReadRoles(connection);
-            // ReadUser();
-            // CreateUser();
-            // UpdateUser();
-            // DeleteUser();
+
+            // createCategory(Database.Connection);
+            createUserRole(Database.Connection);
+            // createPost(Database.Connection);
+            // Load();
+            // Console.ReadKey();
         }
+    }
 
-
-        static void ReadUsers(SqlConnection connection)
+    static void createUserRole(SqlConnection connection)
+    {
+        var userRole = new UserRole()
         {
-            var repository = new UserRepository(connection);
-            var users = repository.GetUsers();
-            foreach (var user in users)
-                Console.WriteLine(user.Name);
-        }
+            UserId = 1,
+            RoleId = 1
+        };
 
-        static void ReadRoles(SqlConnection connection)
+        var repository = new Repository<UserRole>(connection);
+        repository.Create(userRole);
+    }
+
+    static void createCategory(SqlConnection connection)
+    {
+        var category = new Category()
         {
-            var repository = new RoleRepository(connection);
-            var roles = repository.GetRoles();
-            foreach(var role in roles)
-                Console.WriteLine(role.Name);
+            Name = "Desenvolvedor C# .Net",
+            Slug = "desenvolvedor-C#"
+        };
+
+        var repository = new Repository<Category>(connection);
+        repository.Create(category);
+    }
+
+    static void createPost(SqlConnection connection)
+    {
+        var post = new Post()
+        {
+            AuthorId = 1,
+            CategoryId = 1,
+            Body = "Criador de conteúdos C#",
+            Slug = "post-mobile",
+            Summary = "Especialista em conteúdos C#",
+            Title = "Desenvolvimento C#"
+        };
+        var repository = new Repository<Post>(connection);
+        repository.Create(post);
+    }
+    private static void Load()
+    {
+        Console.Clear();
+        Console.WriteLine("Meu Blog");
+        Console.WriteLine("---------------");
+        Console.WriteLine("O que deseja fazer?");
+        Console.WriteLine();
+        Console.WriteLine("1 - Gestão de usuários");
+        Console.WriteLine("2 - Gestão de perfil");
+        Console.WriteLine("3 - Gestão de categoria");
+        Console.WriteLine("4 - Gestão de tag");
+        Console.WriteLine("5 - Vincular perfil/usuário");
+        Console.WriteLine("6 - Vincular post/tag");
+        Console.WriteLine("7 - Relatórios");
+        Console.WriteLine();
+        Console.WriteLine();
+        var option = short.Parse(Console.ReadLine()!);
+
+        switch (option)
+        {
+            // case 1:
+            //     ListTagScreen.Load();
+            //     break;
+            // case 2:
+            //     CreateTagScreen.Load();
+            //     break;
+            // case 3:
+            //     UpdateTagScreen.Load();
+            //     break;
+            case 4:
+                MenuTagScreen.Load();
+                break;
+            default: Load(); break;
         }
-    
     }
 }
---
+++ Screens/TagScreens/ListTagsScreen.cs
@@ -0,0 +1,14 @@
+using Blog.Repositories;
+
+namespace Blog.Screens.TagScreens
+{
+    public static class ListTagScreen
+    {
+        public static void Load()
+        {
+        }
+        private static void List(){
+            // var repository = new Repository<TagScreens>()
+        }
+    }
+}
